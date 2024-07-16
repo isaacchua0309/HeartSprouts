e08bdd4d5cf6cd19b285a2a68c3c0adb8849e6f3
Darren Teo <darrenteo@MacBook-Pro-165.local>
2024-07-16 17:12:21 +0800

    test

--- screens/HOMEscreens/ProfileModal.js
+++ screens/HOMEscreens/ProfileModal.js
@@ -84,10 +84,9 @@ const styles = StyleSheet.create({
   },
   premiumButton: {
     backgroundColor: Colors.green500,
-    padding: 20,
+    padding:12,
     borderRadius: 10,
     alignItems: 'center',
-    marginBottom: 20,
   },
   premiumButtonText: {
     color: Colors.white500,
@@ -110,8 +109,7 @@ const styles = StyleSheet.create({
     color: Colors.white500,
     fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 5,
-    marginTop: 16
+    marginTop: 12
   },
   optionButton: {
     paddingVertical: 15,
